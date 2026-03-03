52b62489e476135a0e3e956012d8ae0224faf5b5
Fabián Huertas <miloh9318@hotmail.com>
2026-03-03 12:17:12 -0500

    manejando los intentos fallidos

--- hanging/src/App.tsx
+++ hanging/src/App.tsx
@@ -1,25 +1,35 @@
+import { useState } from 'react';
 import { letters } from './helpers/letters';
 import './App.css';
 import { HangImage } from './components/HangImage';
 
 function App() {
+
+  const [ attempts, setAttempts ] = useState(0);
+
+  const checkLetter = (letter: string) => {
+    console.log(letter);
+    setAttempts( Math.min( attempts + 1, 9 ) );
+  }
   
   return(
     <div className="App">
       
       {/* Imagenes */}
-      <HangImage imageNumber={ 1 }/>
+      <HangImage imageNumber={ attempts }/>
 
       {/* Palabra oculta */}
       <h3>_ _ _ _ _ _ _ _ _ _</h3>
 
       {/* Contador intentos */}
-      <h3>Intentos: 0</h3>
+      <h3>Intentos: { attempts }</h3>
 
       {/* Botones letra */}
       {
         letters.map( (letter) =>(
-          <button key={ letter }>
+          <button 
+            onClick={ () => checkLetter (letter)}
+            key={ letter }>
             { letter }
           </button>
         ))
