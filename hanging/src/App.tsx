236ec5cb68852ac9f1a5ba089e6bb700f29e7faf
Fabián Huertas <miloh9318@hotmail.com>
2026-03-03 14:36:39 -0500

    mensaje de ganaste o perdiste

--- hanging/src/App.tsx
+++ hanging/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { letters } from './helpers/letters';
 import './App.css';
 import { HangImage } from './components/HangImage';
@@ -8,8 +8,29 @@ function App() {
   const [ word ] = useState('COMPUTADORA');
   const [ hiddenWord, setHiddenWord ] = useState('_ '.repeat( word.length ));
   const [ attempts, setAttempts ] = useState(0);
+  const [ lose, setLose ] = useState( false );
+  const [ won, setWon ] = useState( false );
+
+
+  // determinar si el jugador perdio
+  useEffect( () => {
+    if( attempts >= 9 ) {
+      setLose( true );
+    }
+  }, [ attempts ]);
+
+  // Determinar si la persona gano
+  useEffect(() =>{
+    //console.log(hiddenWord)
+    const currentHiddenWord = hiddenWord.split(' ').join('');
+    if( currentHiddenWord === word ){
+      setWon( true );
+    }
+  }, [ hiddenWord ])
 
   const checkLetter = (letter: string) => {
+
+    if( lose ) return;
     
     if( !word.includes(letter) ) {
       setAttempts( Math.min( attempts + 1, 9 ) );    
@@ -38,6 +59,21 @@ function App() {
 
       {/* Contador intentos */}
       <h3>Intentos: { attempts }</h3>
+
+      {/* Mensaje si perdio */}
+      {
+        ( lose ) 
+          ? <h2>Perdiste! { word }</h2>
+          : ''
+      }
+
+      {/* Mensaje si gano */}
+      {
+        ( won ) 
+          ? <h2>Ganaste! { word }</h2>
+          : ''
+      }
+
 
       {/* Botones letra */}
       {
